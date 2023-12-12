6e3ccea6a75d42d634a94bafbfc81cbeed6f943a
Ivorusso <129088802+ivoorusso@users.noreply.github.com>
2023-12-12 17:49:18 -0300

    ngrok screen

--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -16,7 +16,7 @@ function ChatComponent() {
       let inputToSend = input
       setInput("");
       try {
-        const response = await axios.post("https://3ee5-2600-1f18-762e-6b00-f71e-4c67-c445-5932.ngrok-free.app/test", {
+        const response = await axios.post("https://11b7-2600-1f18-762e-6b00-f71e-4c67-c445-5932.ngrok-free.app/chat", {
           question: inputToSend,
         });
 
