3d6d64e782eee028fa210505ee3f85a9ffd396ac
Fabricio Montivero <fabricio@setandforget.io>
2024-01-04 14:30:43 -0300

    se agrego opciones de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'ross-green': '#00B2A4',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
