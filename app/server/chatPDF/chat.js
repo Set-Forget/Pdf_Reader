044d0beaafa27425a27a5c1f2627cdbf72bea530
Fabricio Montivero <fabricio@setandforget.io>
2024-01-29 17:01:19 -0300

    se agregaron los metodos de conexión con el apikey

---
+++ app/server/chatPDF/chat.js
@@ -0,0 +1,34 @@
+async function chatPDFMsg() {
+    const req = {
+        referenceSources: true,
+        sourceId: "src_L6XL9ZzNo8HrAof2Hkh7s",
+        messages: [
+          {
+            role: "user",
+            content: "what are the verbal VEO requirements for military personnle?",
+          },
+        ],
+    };
+
+    const apiUrl = "https://api.chatpdf.com/v1/chats/message"
+    const content = await fetch(apiUrl, {
+        "method": "POST",
+        "body" : JSON.stringify(req),
+        "headers": {
+            "x-api-key": process.env.CHATPDF_API_KEY,
+            "Content-Type": "application/json",
+          },
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log("Result:", data);
+        return data.content
+    })
+    .catch((error) => {
+        console.log("Error:", error);
+    });
+
+    return content
+}
---
+++ app/server/chatPDF/delete.js
@@ -0,0 +1,25 @@
+async function chatPDFDelete(...srcs) {
+    const apiUrl = "https://api.chatpdf.com/v1/sources/delete"
+    const opt = {
+        "method": "POST",
+        "body" : JSON.stringify({
+            sources: srcs,
+          }),
+        "headers": {
+            "x-api-key": process.env.CHATPDF_API_KEY,
+            "Content-Type": "application/json",
+          },
+    }
+
+    const res = await fetch(apiUrl, opt)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Source ID:", data);
+        return data
+    })
+    .catch((error) => {
+        console.log("Error:", error.message);
+    });
+    
+    return res
+}
