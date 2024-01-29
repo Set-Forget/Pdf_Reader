3098b1139a0ae2103119e3a8cef1eec3b83844e2
Juan Canneva <juancanneva@setandforget.io>
2024-01-29 16:59:59 -0300

    added dark mode classes

--- app/client/components/Header.jsx
+++ app/client/components/Header.jsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Header() {
     return(
-      <div className="flex justify-center items-center p-4 bg-white shadow-md">
+      <div className="flex justify-center items-center p-4 shadow-md bg-white dark:bg-slate-700 dark:text-white">
         <Link href="/" className="flex items-center">
           <img
             src="favicon.ico"
--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from '@client/components/Header'
-import { AppContext } from '@/client/context/FilesContext';
-import { Toaster } from 'sonner';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@client/components/Header";
+import { AppContext } from "@/client/context/FilesContext";
+import { Toaster } from "sonner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'PDF ChatBot',
-  description: 'Chat with OpenAI',
-  favicon: './favicon.ico'
-}
+  title: "PDF ChatBot",
+  description: "Chat with OpenAI",
+  favicon: "./favicon.ico",
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -21,7 +21,12 @@ export default function RootLayout({ children }) {
           <div className="flex justify-center items-center h-screen bg-ross-green">
             <div className="w-full h-full flex flex-col">
               <Header />
-              <div className="flex-1 overflow-auto p-4 flex flex-col">
+              <div
+                className="flex-1 overflow-auto p-4 flex flex-col"
+                style={{
+                  background: "linear-gradient(180deg, #05293e, #033652)",
+                }}
+              >
                 {children}
               </div>
             </div>
@@ -29,5 +34,5 @@ export default function RootLayout({ children }) {
         </AppContext>
       </body>
     </html>
-  )
+  );
 }
